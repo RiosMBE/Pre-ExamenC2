fb3fe30ac2795c2495bdf0ad034d811e18a375c9
Brian Rios <briosm0601@gmail.com>
2023-03-06 15:00:32 -0700

    Correccion de errores

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 const http = require("http");
 const express = require("express");
-const app = express();
 const bodyparser = require("body-parser");
 const misRutas = require("./router/index");
 const path = require("path");
 
-
+const app = express();
 app.set('view engine', "ejs")
 app.use(express.static(__dirname + '/public'));
 app.engine('html', require('ejs').renderFile)
@@ -13,13 +12,12 @@ app.engine('html', require('ejs').renderFile)
 app.use(bodyparser.urlencoded({extended:true}));
 app.use(misRutas)
 
-// Escuchar al servidor por el puerto 503
 const puerto = 503;
 
-app.use((req, res, next) => {
+app.use((req, res, next) =>{
     res.status(404).sendFile(__dirname + '/public/error.html');
 })
 
-app.listen(puerto, () => {
-    console.log("iniciando puerto 500")
+app.listen(puerto, ()=>{
+    console.log("iniciando puerto 503")
 })
--- router/index.js
+++ router/index.js
@@ -2,50 +2,42 @@ const express = require("express");
 const router = express.Router();
 const bodyparser = require("body-parser");
 
-router.get('/index', (req, res) => {
-    const num_boleto = req.query.num_boleto;
-    const destino = req.query.destino;
-    const nombre = req.query.nombre;
-    const edad = req.query.edad;
-    const tipo_viaje = req.query.tipo_viaje;
-    const precio = req.query.precio;
-    res.render('index.html', num_boleto, destino, nombre, edad, tipo_viaje, precio);
-});
+module.exports = router;
 
-router.post('/index', (req, res) => {
-    const num_boleto = req.body.num_boleto;
-    const destino = req.body.destino;
-    const nombre = req.body.nombre;
-    const edad = req.body.edad;
-    const tipo_viaje = req.body.tipo_viaje;
-    const precio = req.body.precio;
-
-    const subtotal = precio;
-
-    if (tipo_viaje == 2) {
-        subtotal += precio * 0.8;
+router.get("/index", (req, res)=>{
+    const valores = {
+        numBoleto: req.query.numBoleto,
+        destino: req.query.destino,
+        cliente: req.query.cliente,
+        edad: req.query.edad,
+        tipoViaje: req.query.tipoViaje,
+        precio: req.query.precio,
+        total: req.query.total
     }
+    res.render('index.html', valores)
+})
 
-    let descuento = 0;
-    if (edad >= 60) {
-        descuento = subtotal * 0.5;
+router.post("/index", (req, res)=>{
+    const valores = {
+        numBoleto: req.body.numBoleto,
+        destino: req.body.destino,
+        cliente: req.body.cliente,
+        edad: req.body.edad,
+        tipoViaje: req.body.tipoViaje,
+        precio: req.body.precio,
+        total: req.body.total
     }
+    res.render('index.html', valores)
+})
 
-    const impuesto = subtotal * 0.16;
-    const total = (subtotal) - descuento + impuesto;
-
-res.render('index.html', {
-        num_boleto,
-        destino,
-        nombre,
-        edad,
-        tipo_viaje,
-        precio,
-        subtotal,
-        impuesto,
-        descuento,
-        total,
-    });
-});
-
-module.exports=router;
+function limpiarCampos(){
+    document.getElementById("numBoleto").value = "";
+    document.getElementById("destino").value = "";
+    document.getElementById("cliente").value = "";
+    document.getElementById("edad").selectIndex = 0;
+    document.getElementById("precio").value = "";
+    document.getElementById("subtotal").value = "";
+    document.getElementById("impuesto").value = "";
+    document.getElementById("descuento").value = "";
+    document.getElementById("preciof").value = "";
+}
